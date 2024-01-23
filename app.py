902be53c62940335a0213b94cd0b3607573093b0
aniket679 <156010605+aniket679@users.noreply.github.com>
2024-01-23 10:19:44 +0000

    .

--- app.py
+++ app.py
@@ -1,12 +1,38 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [{
+    'id': 1,
+    'title': 'Python Developer',
+    'location': 'Pune, India',
+    'salary': 'Rs. 3,00,000'
+}, {
+    'id': 2,
+    'title': 'Java Developer',
+    'location': 'Delhi, India',
+    'salary': 'Rs. 4,00,000'
+}, {
+    'id': 3,
+    'title': 'FrontEnd Engineer',
+    'location': 'Remote',
+}, {
+    'id': 4,
+    'title': 'BackEnd Engineer',
+    'location': 'San Francisco, USA',
+    'salary': '$120,000'
+}]
+
 
 @app.route("/")
-def hello_world():
-  return "<p>Hello, World!</p>"
+def index():
+  return render_template('home.html', jobs=JOBS)
 
 
-if __name__ == '__main__':
-  app.run(host='0.0.0.0', debug=True)
+@app.route("/jobs")
+def list_jobs():
+  return jsonify(JOBS)
+
+
+if (__name__) == "__main__":
+  app.run(debug=True)
